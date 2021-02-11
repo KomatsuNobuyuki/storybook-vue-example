3611c0a1c604b6978f78293f21429fa1a6a84f5b
Komatsu Nobuyuki <abr0811@icloud.com>
2021-02-11 18:33:42 +0900

    ボタン追加

--- _src/js/App.vue
+++ _src/js/App.vue
@@ -1,9 +1,28 @@
 <template>
-  <h1>Hello</h1>
+  <div>
+    <h1>Hello Vue</h1>
+    <div class="button-wrap">
+      <my-button>テキスト</my-button>
+    </div>
+    <div class="button-wrap">
+      <my-button primary block>テキスト</my-button>
+    </div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
-h1 {
-  color: blue;
+.button-wrap {
+  margin-top: 15px;
 }
 </style>
+
+<script>
+import '../css/reset.scss';
+import MyButton from './components/Button.vue';
+
+export default {
+  components: {
+    MyButton,
+  }
+}
+</script>
